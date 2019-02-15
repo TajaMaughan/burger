2f1b0ed783489a405b9b773437183c65e3185257
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-14 18:25:44 -0700

    file structure

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//MySQL connection
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bugers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//exports connection of our ORM to use.
+module.exports = connection;
